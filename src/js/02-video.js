24ffc9e113e4d31c13203b955d7fbd7e862cb08d
julichka-sun07 <112427883+julichka-sun07@users.noreply.github.com>
2023-05-04 21:59:30 +0300

    hw-1-008

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+let onPlay = function({seconds}) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+};
